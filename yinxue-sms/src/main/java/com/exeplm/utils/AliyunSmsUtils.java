00168718a80b18d1170e110f83b6ee8df1f76b93
hupujrsgit <57165757+hupujrsgit@users.noreply.github.com>
2022-07-14 17:32:59 +0800

    Update AliyunSmsUtils.java

--- yinxue-sms/src/main/java/com/exeplm/utils/AliyunSmsUtils.java
+++ yinxue-sms/src/main/java/com/exeplm/utils/AliyunSmsUtils.java
@@ -17,8 +17,7 @@ public class AliyunSmsUtils {
 
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAI5tQqM3y8wgPwbSuBanyv";
-    static final String accessKeySecret = "qPZlgiVeER39TeuuarPJKHcfO5ewmS";
+    
 
     public static SendSmsResponse sendSms(String telephone, String code) throws Exception {
 
